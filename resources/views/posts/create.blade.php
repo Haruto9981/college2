8c6b7a580bbb8e9fd4e02de45748d5fd72d5e30e
Haruto9981 <aketaharuto46@icloud.com>
2024-01-31 20:46:30 +0900

    test

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,20 +6,26 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
+        <!-- formタグにenctypeを追加 -->
+        <form action="/posts" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
-                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
             </div>
-            <input type="submit" value="保存"/>
+            <!-- ここから追加 -->
+            <div class="image">
+                <input type="file" name="image">
+            </div>
+            <!-- ここまで追加 -->
+            <input type="submit" value="store"/>
         </form>
-        <div class="back">[<a href="/">back</a>]</div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
     </body>
 </html>
